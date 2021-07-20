ec73cf455528c80bf23ded2b966c663b3c5e85c3
Juan Soto <sotoj88515@gmail.com>
2021-07-19 20:42:06 -0500

    First commit

--- snake/app.js
+++ snake/app.js
@@ -1,46 +1,176 @@
 let boardElem = document.querySelector('#board');
+let nameDisplay = document.querySelector('#playerName');
+let resetButton = document.querySelector('.reset');
+let score = document.querySelector('#score');
+let bodyPart;
+let play = 0;
+let selBoard = document.querySelectorAll(".board, .cell");
 
-// state
+
 let initialState = {};
 
+
 function buildInitialState() {
-        initialState.snake = {
-        body: [ [10, 5], [10, 6], [10, 7], [10, 8] ],
-        nextDirection: [1, 0]
-      }
-      initialState.board = []
+    initialState.player = '';
+    initialState.score = 0;
+    initialState.direction = "";
+    initialState.snakePositions = [168, 169, 170, 171];
+    initialState.applePosition = 100;
 }
 
-// render
-function renderState() {
-    boardElem.innerText = '';
-    let alternator = true;
+
+nameDisplay.addEventListener('click', function(event){
+    if(event.target.className !== 'enter')
+    return;
+    const playerInput = document.querySelector('input[name = player]');
+    initialState.player = playerInput.value;
+    renderPlayer();
+    play++;
+    
+})
+
+function resetBoard(){
+    for(let i = 0; i < selBoard.length; i ++){
+        selBoard[i].removeAttribute('id');
+    }
+}
+
+function renderScore(){
+    score.innerHTML = `${initialState.score}`
+}
+function renderBoard(){
     let gridBox;
-    for(let i = 0; i < 255; i ++){
-        if(alternator === true){
-            gridBox = document.createElement('div');
-            gridBox.classList.add('oddBox');
-            gridBox.innerHTML = ' j';
-            boardElem.appendChild(gridBox);
-            alternator = false;
-        }
-        else{
-            gridBox = document.createElement('div');
-            gridBox.classList.add('evenBox');
-            gridBox.innerHTML = ' j';
-            boardElem.appendChild(gridBox);
-            alternator = true;
-        }
+    for(let i = 0; i < 225; i++){
+        gridBox = document.createElement('div');
+        gridBox.classList.add('cell');
+        boardElem.appendChild(gridBox);
+    }
+}
+
+function renderState() {
+
+    renderScore();
+    
+    let bodyPart; 
+    
+    const tiles = document.querySelectorAll(".board, .cell");
+    
+    const apple = tiles[initialState.applePosition];
+    apple.setAttribute('id','apple');
+    for(let i = 0; i < initialState.snakePositions.length; i++){
+        let snakePiece = initialState.snakePositions[i];
+        bodyPart = tiles[snakePiece];
+        bodyPart.setAttribute('id','snake');
     }
 
 }
-
-// maybe a dozen or so helper functions for tiny pieces of the interface
-
-// listeners
-function onBoardClick() {
-  // update state, maybe with another dozen or so helper functions...
-
-  renderState() // show the user the new state
+function renderPlayer(){
+    let text;
+    if(!initialState.player){
+        text = `
+            <input name = "player"
+            placeholder="Enter Name!">
+            <button class = "enter">Start</button>`
+    } else {
+        text = initialState.player
+    }
+    nameDisplay.innerHTML = text;
 }
-renderState();
+function increaseScore(){
+    initialState.score++;
+    let newPos = Math.floor(Math.random() * 226);
+    initialState.applePosition = newPos;
+
+}
+
+function tick() {
+    // this is an incremental change that happens to the state every time you update...
+    
+    let cells = document.querySelectorAll(".board, .cell");
+    if(initialState.direction === "ArrowRight"){
+        let lastIndex = initialState.snakePositions.length - 1;
+        let nextStep = initialState.snakePositions[lastIndex] + 1;
+        let snakeTail = initialState.snakePositions[0];
+        if(nextStep === initialState.applePosition){
+            initialState.snakePositions.push(nextStep);
+            increaseScore();
+            
+        }
+        else{
+            cells[snakeTail].removeAttribute('id');
+            initialState.snakePositions.splice(0,1);
+            initialState.snakePositions.push(nextStep);
+            }
+        }
+    else if(initialState.direction === "ArrowLeft"){
+        let lastIndex = initialState.snakePositions.length - 1;
+        let nextStep = initialState.snakePositions[lastIndex] - 1;
+        let snakeTail = initialState.snakePositions[0];
+        if(nextStep === initialState.applePosition){
+            initialState.snakePositions.push(nextStep);
+            increaseScore();
+            
+        }
+        else{
+            cells[snakeTail].removeAttribute('id');
+            initialState.snakePositions.splice(0,1);
+            initialState.snakePositions.push(nextStep);
+        }
+    }
+    else if(initialState.direction === "ArrowUp"){
+        let lastIndex = initialState.snakePositions.length - 1;
+        let nextStep = initialState.snakePositions[lastIndex] - 15;
+        let snakeTail = initialState.snakePositions[0];
+        if(nextStep === initialState.applePosition){
+            initialState.snakePositions.push(nextStep);
+            increaseScore();
+            
+        }
+        else{
+            cells[snakeTail].removeAttribute('id');
+            initialState.snakePositions.splice(0,1);
+            initialState.snakePositions.push(nextStep);
+        }
+    }
+    else if(initialState.direction === "ArrowDown"){
+        let lastIndex = initialState.snakePositions.length - 1;
+        let nextStep = initialState.snakePositions[lastIndex] + 15;
+        let snakeTail = initialState.snakePositions[0];
+        if(nextStep === initialState.applePosition){
+            initialState.snakePositions.push(nextStep);
+            increaseScore();
+            
+        }
+        else{
+            cells[snakeTail].removeAttribute('id');
+            initialState.snakePositions.splice(0,1);
+            initialState.snakePositions.push(nextStep);
+        }
+    }
+    let limit = initialState.snakePositions.length - 1;
+    for(let i = 0; i < limit; i++){
+        if(initialState.snakePositions[limit] === initialState.snakePositions[i]){
+            nameDisplay.innerHTML = `<h1 id="playerName"> You Lose! </h1>`
+            setInterval(tick, 1000/ 0.00000010);
+        }
+    }
+    renderState()
+  }
+    renderPlayer();
+    buildInitialState();
+    renderBoard();
+    setInterval(tick, 1000 / 10);
+  
+    document.addEventListener('keydown', function(event){
+     if(event.code && play === 1){
+        initialState.direction = event.code;
+     }
+    })
+    resetButton.addEventListener('click', function(){
+        resetBoard();
+        renderPlayer();
+        renderState();
+        buildInitialState();
+        renderBoard();
+    })
+  
